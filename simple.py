4f6da311256672a18a39f121ecb69746171b3c3e
Maria.Keshish <mari.black@list.ru>
2021-09-16 16:19:36 +0300

    Added WELCOME, DEAR

---
+++ simple.py
@@ -0,0 +1,11 @@
+print ('Welcome, my dear')
+x=input('Введите число: ')
+y=int(x)
+z=y+1
+f=y-1
+a=str(z)
+b=str(f)
+print('За числом '+x+' следует число '+a+'.')
+print('Перед числом '+x+' следует число '+b+'.')
+
+
